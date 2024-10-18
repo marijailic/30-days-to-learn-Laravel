765d73568ac753aa72ce0303a6a78eb02270dd14
Marija <marija@MacBook-Air.home>
2024-10-18 17:06:16 +0200

    17 Always Validate. Never Trust the User.

--- example/app/Models/Job.php
+++ example/app/Models/Job.php
@@ -10,8 +10,8 @@ class Job extends Model
     use HasFactory;
 
     protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary'];
-
+//    protected $fillable = ['employer_id', 'title', 'salary'];
+    protected $guarded = [];
     public function employer()
     {
         return $this->belongsTo(Employer::class);
